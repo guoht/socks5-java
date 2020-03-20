bb6696d4bf51cc9291fa2f7c3dc0830304f592c8
guohaitao02 <guohaitao02@58.com>
2020-03-20 13:13:51 +0800

    支持ssl

--- socks5-java-netty/src/main/java/com/github/bpazy/socks5/netty/NettySocks5.java
+++ socks5-java-netty/src/main/java/com/github/bpazy/socks5/netty/NettySocks5.java
@@ -1,11 +1,17 @@
 package com.github.bpazy.socks5.netty;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.ssl.SslHandler;
+
+import javax.net.ssl.SSLEngine;
 
 /**
  * @author ziyuan
@@ -22,7 +28,18 @@ public class NettySocks5 {
             b.group(bossGroup, workerGroup)
                     .channel(NioServerSocketChannel.class)
                     .handler(new LoggingHandler(LogLevel.INFO))
-                    .childHandler(new SocksServerInitializer());
+                    .childHandler(new SocksServerInitializer())
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            SSLEngine sslEngine = SSLContextFactory.getSslContext().createSSLEngine();
+                            sslEngine.setUseClientMode(false);
+                            ch.pipeline().addLast(new SslHandler(sslEngine));
+                            ch.pipeline().addLast("http-decoder", new HttpServerCodec());
+                            ch.pipeline().addLast(new HttpsSeverHandler());
+                        }
+
+                    });
             b.bind(PORT).sync().channel().closeFuture().sync();
         } finally {
             bossGroup.shutdownGracefully();
